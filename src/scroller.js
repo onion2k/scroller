9b9f66275b00329d0f28f40fa525e77007f3f081
Chris <chris@usablehq.com>
2017-12-06 06:48:11 +0000

    Basic scroller working

--- src/scroller.js
+++ src/scroller.js
@@ -12,18 +12,48 @@ c.width = 800;
 c.height = 600;
 let ctx = c.getContext('2d');
 
+let map = [];
+let text = "THIS IS SCROLLY TEXT";
+let offset = 800;
+let s = 0;
 
 fontPromise.then(()=>{
     ctx.fillRect(0,0,800,600);
 
     let x, y;
-    for (let i=0; i<36; i++) {
+    for (let i=0; i<26; i++) {
 
         x = (i%9) * 32;
         y = Math.floor(i/9) * 32;
-
-        ctx.drawImage(font, x,y, 32,32, ((i%9)*50)+50,Math.floor(i/9)*50+50, 32,32);
+        map[65+i] = [x,y];
 
     }
 
+    for (let i=0; i<10; i++) {
+        
+        x = ((i+26)%9) * 32;
+        y = Math.floor((i+26)/9) * 32;
+        map[48+i] = [x,y];
+
+    }
+
+    animate();
+    
 });
+
+function animate(){
+
+    ctx.fillRect(0,0,800,600);
+    
+    let l;
+    offset -= 5;
+    s += 0.05;
+    if (offset < -1*text.length*35) { offset = 800 }
+    for (let c=0; c<text.length; c++) {
+        l = text.charCodeAt(c);
+        if (l===32) { continue; }
+        ctx.drawImage(font, map[l][0],map[l][1], 32,32, offset+c*35,275+(50*Math.sin((c/4)+s)), 32,32);
+    }
+
+    requestAnimationFrame(animate);
+}
